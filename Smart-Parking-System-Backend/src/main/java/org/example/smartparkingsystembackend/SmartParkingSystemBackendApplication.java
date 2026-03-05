bbe03f2e4a2ee498ebee5ea20f5431674c6bf242
CAME <punchihewadevindi@gmail.com>
2026-03-05 16:36:52 +0530

    ui is ok

---
+++ Smart-Parking-System-Backend/src/main/java/org/example/smartparkingsystembackend/SmartParkingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.smartparkingsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartParkingSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartParkingSystemBackendApplication.class, args);
+    }
+
+}
---
+++ Smart-Parking-System-Backend/src/test/java/org/example/smartparkingsystembackend/SmartParkingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartparkingsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
